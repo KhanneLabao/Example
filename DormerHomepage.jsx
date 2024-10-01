a75c0de4841fd83f61c9de9070d8a7d873a657e5
KhanneLabao <labaokhanne7@gmail.com>
2024-10-01 17:43:07 +0800

    First Commit

---
+++ DormerHomepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import "../index.css"
+import Hero from '../components/Hero';
+import WelcomeMessage from '../components/WelcomeMessage';
+
+
+const DormerHomepage = () => {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default DormerHomepage
